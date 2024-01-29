98b7525b21ad12f3c5f21ea3ae25a7a81d688eff
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2024-01-29 09:18:46 +0530

    Add files via upload

---
+++ 2_8.py
@@ -0,0 +1,18 @@
+#8
+
+a='The Rajkot is Good city to leave'
+print(a)
+#1)Display the first Character of the String using negative index
+print(a[0])
+#2)Display the first Character of the String using negative index
+print(a[:-1])
+#3)Display the 'Rajkot is Good city'.
+print(a[4:23])
+#1)Display the last Character of the String
+print(a[-1:])
+Output:-
+The Rajkot is Good city to leave
+T
+The Rajkot is Good city to leav
+Rajkot is Good city
+e
