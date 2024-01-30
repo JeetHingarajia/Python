22b635bd816f3dab6acd06b9d0c1f0f458e7cdc1
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2024-01-30 11:38:23 +0530

    Add files via upload

---
+++ 2_10.py
@@ -0,0 +1,16 @@
+#10. Create bytearray, enter some values and perform the following:
+
+a=[1,2,3,4,6,8,10,12]
+print(a)
+
+# i). Replace the 3rd element with 7, 
+b=bytearray(a)
+b[2]=7
+print(b[2])
+ #ii). Display the 5th element.
+print(b[4])
+
+Output:-
+[1, 2, 3, 4, 6, 8, 10, 12]
+7
+6
---
+++ 2_9.py
@@ -0,0 +1,10 @@
+#9. Create bytes, enter some values and display all elements.
+
+a=[2,4,24,1,3,5,6,9]
+print(a)
+b=bytes(a)
+print(b)
+
+Output:-
+[2, 4, 24, 1, 3, 5, 6, 9]
+b'\x02\x04\x18\x01\x03\x05\x06\t'
