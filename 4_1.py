c40af321910f16d557327c0a9e2214539d310511
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2024-02-20 11:37:09 +0530

    Add files via upload

---
+++ 4_1.py
@@ -0,0 +1,104 @@
+# #1
+# a=int(input('Enter Value of A:'))
+# b=int(input('Enter Value of B:'))
+
+# if a>b:
+#     print(a,'The Value Of A Is Bigger Than B')
+#     print(b,'The Value Of B Is Smaller Than A')
+# else:
+#     print(a,'The Value Of A Is Smaller Than B')
+#     print(b,'The Value Of B Is Bigger Than A')
+
+#2
+# a=int(input('Enter Value of A:'))
+
+# if a%5==0:
+#     print('Number is Divisible by 5')
+# else:
+#     print('Number is not Divisible by 5')
+
+#3
+# a=int(input('Enter Value of A:'))  
+
+# if a>=0 and a<=100:
+#     print('The Value Of A is Between 0 and 100')
+# else:
+#     print('The Value Of A is not Between 0 and 100')
+
+#4
+# a=input('Enter Value of A:')
+# b=len(a)
+# print('Number is A:')
+
+# if b==4:
+#     print('The Number',a,'is 4 digit Number')
+# else:
+#     print('The Number',a,'is not 4 digit Number')
+
+#5
+# a=int(input('Enter Value of A:'))
+# if a==1:
+#     print('Sunday')
+# elif a==2:
+#     print('Monday')
+# elif a==3:
+#     print('Tuesday')
+# elif a==4:
+#     print('Wednesday')
+# elif a==5:
+#     print('Thursday')
+# elif a==6:
+#     print('Friday')
+# elif a==7:
+#     print('Saturday')
+# else:
+#     print('Enter Valid No.')
+
+#6
+# a=int(input('Enter Value of A:'))
+# b=int(input('Enter Value of B:'))  
+# c=int(input('Enter Value of C:'))
+# # add=a+b
+# # sub=a-b
+# # mul=a*b
+# # div=a/b
+
+# if c==1:
+#     print('Addition',a+b)
+# elif c==2:
+#     print('Substraction',a-b)
+# elif c==3:
+#     print('Multiplication',a*b)
+# elif c==4:
+#     print('Division',a/b)
+# else:
+#     print('Invalid Number')
+
+#7
+
+
+
+
+
+#8
+# a=int(input('Enter Value of A:'))
+# b=1
+# print('The Table Of : ',a)
+
+# while b<=10:
+#     ans=a*b
+#     print(a,'X',b,'=',ans)
+#     b+=1
+
+#9
+# a=1
+# while(a<=10):
+#     Sqr=a**2
+#     cub=a**3
+#     print('Square is',Sqr, 'and Cube is',cub)
+#     a+=1
+
+#10
+# a=str(input('Enter String:'))
+# b=str.upper(a)
+# print(b)
